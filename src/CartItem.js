8324cf8c7725c80b671965023f1ff53776035304
ip-nandwani <nandwani.ind@gmail.com>
2022-02-20 12:30:53 +0530

    used states with class components

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,37 +1,50 @@
 import React from 'react';
 
-const CartItem = () => {
-  return (
-    <div className="cart-item">
-      <div className="left-block">
-        <img style={styles.image} />
-      </div>
-      <div className="right-block">
-        <div style={{ fontSize: '25px' }}>Phone</div>
-        <div style={{ color: '#777' }}>Rs. 1999</div>
-        <div style={{ color: '#777' }}>Qty: 1</div>
-        <div className="cart-item-actions">
-          {/* buttons */}
-          <img
-            alt="increase"
-            className="action-icons"
-            src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
-          />
-          <img
-            alt="decrease"
-            className="action-icons"
-            src="https://cdn-icons.flaticon.com/png/512/3114/premium/3114894.png?token=exp=1645302273~hmac=051a29408a31fdd9cb1996583415af9f"
-          />
-          <img
-            alt="delete"
-            className="action-icons"
-            src="https://cdn-icons.flaticon.com/png/512/4387/premium/4387288.png?token=exp=1645302406~hmac=e886891bdebb4b02b34cd95d454d2e5c"
-          />
+class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 999,
+      title: 'Smartphone',
+      qty: 1,
+      img: '',
+    };
+  }
+
+  render() {
+    const { price, title, qty } = this.state;
+    return (
+      <div className="cart-item">
+        <div className="left-block">
+          <img alt="" style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={{ fontSize: '25px' }}> {title} </div>
+          <div style={{ color: '#777' }}>Rs. {price} </div>
+          <div style={{ color: '#777' }}>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons.flaticon.com/png/512/3114/premium/3114894.png?token=exp=1645302273~hmac=051a29408a31fdd9cb1996583415af9f"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons.flaticon.com/png/512/4387/premium/4387288.png?token=exp=1645302406~hmac=e886891bdebb4b02b34cd95d454d2e5c"
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 const styles = {
   image: {
